02d7439157b5f8f4f0e8baa654c2def4cbb2f3e4
circlefour <0100.circle@gmail.com>
2023-12-02 17:53:50 -0500

    can it grab back cam

--- js/index.js
+++ js/index.js
@@ -1,19 +1,25 @@
+const vid = document.getElementById('player');
+
 // chack device support
 if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
     console.log('media devices exist');
 }
 
-// log what devices are available to access
-const devices = navigator.mediaDevices.enumerateDevices();
-console.log(devices);
+const constraints = {
+    video: {
+        width: 320,
+        height: 180,
+        facingMode: 'environment',
+    }
+}
 
-//const constraints = {
-//    video: {
-//        width: 320,
-//        height: 180,
-//        facingMode: 'environment',
-//    }
-//}
-//
-//// request user permission
-//navigator.mediaDevices.getUserMedia(constraints)
+async function initCam() {
+    try {
+        const vidStream = await navigator.mediaDevices.getUserMedia(constraints);
+    vid.srcObject = vidStream;
+    } catch (err) {
+        console.log('access to camera denied');
+    }
+}
+
+initCam();
